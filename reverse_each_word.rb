be0b3df3a2a006bfe9a3cc96db6917ad67bf8e07
michael hussain <smhussain_1@yahoo.com>
2019-01-05 21:28:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 
 string = "Hello there, and how are you?"
-def reverse_each_word(string)
-  new_array = string.split(" ")
-  new_array.each do |word|
+def reverse_each_word(string)           
+  new_array = string.split(" ")     # the .split converts the string into an array with a "" around each element.
+  new_array.each do |word|          # goes thru each elemnt of the array and prints it backwards overwriting the old element, with a ""
     print word.reverse! + " "
   end
 end
@@ -11,10 +11,11 @@ reverse_each_word(string)
 
 
 string = "Reverse each word?"
-def reverse_each_word(string)
-  new_array = string.split(" ")
-  new_array.collect { |word| word.reverse! + " " }
-  new_array.join(" ")
+def reverse_each_word(string)   
+  new_array = string.split(" ")   # converts string to an array
+  new_array.collect               # creates NEW array 
+  { |word| word.reverse! + " " }  # reverses each element in the array, adding a ""
+  new_array.join(" ")             # converts the array back into a string, encapsulating the string with ""
     # print word.reverse! + " "
 end
 
